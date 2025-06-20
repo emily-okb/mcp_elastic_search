232d8f92ed3a3668b331c6f5d36679025e4a9b51
emily-okb <emily.okabe@newsbreak.com>
2025-06-20 00:45:45 +0000

    Add sparse vector embedding

--- sparse_vec.py
+++ sparse_vec.py
@@ -1,26 +1,48 @@
 import requests, time
+import tqdm
+import aiohttp
+import asyncio
 
 
-def call_sparse_vec_api(self, batch_text: list[str]):
+async def fetch_sparse_vec(session, batch_text):
     data = {
         "inputs": batch_text,
     }
-    for _ in range(3):
-        try:
-            resp = requests.post("http://10.12.160.250:9200/embed_sparse", json=data, timeout=120)
-            if resp.status_code == 200:
-                res_li = resp.json()
-                assert len(batch_text) == len(res_li)
-                return res_li
+    for _ in range(1):
+        async with session.post("http://10.12.160.9:8084/embed_sparse", json=data) as resp:
+            if resp.status == 200:
+                result = await resp.json()
+                if result:  # Check if the result is empty
+                    return result
+                else:
+                    print("Received empty response")
+                    return None
+            elif resp.status == 413:
+                print("Error 413")
+                await asyncio.sleep(0.2)
             else:
-                continue
-        except Exception as e:
-            print("exception:", e)
-            time.sleep(0.2)
-            continue
+                print(f"Error: Received status code {resp.status}")
+                return None
     return None
 
 
+async def call_sparse_vec_api(batch_text, batch_size=128):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        # Split the text into smaller batches and create tasks
+        for batch in batch_split(batch_text, batch_size):
+            tasks.append(fetch_sparse_vec(session, batch))
+        
+        results = await asyncio.gather(*tasks)  # Execute all requests concurrently
+        return [result for result in results if result is not None]
+
+
+def batch_split(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 if __name__ == "__main__":
+
     ret = call_sparse_vec_api(["This is a test document.", "Another test document for sparse vector enrichment."])
     print(ret)
--- test_es.py
+++ test_es.py
@@ -1,6 +1,8 @@
 import json
 import os
 from elasticsearch import Elasticsearch
+from sparse_vec import call_sparse_vec_api
+import asyncio
 
 
 def create_index(es, index_name, body):
@@ -10,34 +12,64 @@ def create_index(es, index_name, body):
             es.indices.create(index=index_name, body=body)
             print(f"Index '{index_name}' created.")
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error creating index: {e}")
     else:
         print(f"Index '{index_name}' already exists.")
 
 
+def get_text_to_embed(json_data):
+    name = json_data.get("id").split("/", 1)[-1]
+    text_to_embed = json_data.get("about")
+    tools = json_data.get("tools")
+    for tool in tools:
+        description = tool.get("description")
+        if description:
+            text_to_embed += " - " + description
+    text_to_embed = text_to_embed.strip()
+    if len(text_to_embed) == 0:
+        text_to_embed = name
+    return text_to_embed
+
+
 def add_docs_to_index(es, index_name, doc_path):
+    all_data = []
+    text_to_embed = []
     operations_list = []
     for filename in os.listdir(doc_path):
         fullpath = os.path.join(doc_path, filename)
         if os.path.isfile(fullpath) and filename.lower().endswith(".json"):
             with open(fullpath, 'r') as file:
                 data = json.load(file)
-            operations_list.append({
-                "index": {
-                    "_index":index_name
-                }
-            })
-            operations_list.append(data)
+                text_to_embed.append(get_text_to_embed(data))
+                all_data.append(data)
+
+    BATCH_SIZE = 1
+
+    embeddings = asyncio.run(call_sparse_vec_api(text_to_embed, BATCH_SIZE))
+
+    assert len(embeddings) == len(all_data), "Failed to get all embeddings"
+
+    for i, data in enumerate(all_data):
+        data['sparse_embed'] = embeddings[i][0]
+    
+        operations_list.append({
+            "index": {
+                "_index":index_name
+            }
+        })
+        operations_list.append(data)
 
     resp = es.bulk(operations=operations_list)
     assert not resp["errors"], "Error adding documents."
+    
 
 
 def validate_doc_format(doc):
     EXPECTED_KEYS = {
         "id": str,
         "about": str,
-        "tools": list
+        "tools": list,
+        "sparse_embed": list,
     }
     EXPECTED_TOOL_KEYS = {
         "name": (str, list, type(None)),
@@ -47,24 +79,38 @@ def validate_doc_format(doc):
         "role": str,
         "parameters": dict,
     }
+    EXPECTED_SPARSE_KEYS = {
+        "index": int,
+        "value": float,
+    }
 
     for key, expected_type in EXPECTED_KEYS.items():
         if key not in doc or not isinstance(doc[key], expected_type):
-            print(doc[key])
+            print(key)
             return False
 
     if "tools" in doc:
         for item in doc["tools"]:
             for tools_key in item:
                 if tools_key not in EXPECTED_TOOL_KEYS.keys():
+                    print(f"tools: {tools_key}")
                     return False
+    
+    if "sparse_embed" in doc:
+        for key, expected_type in EXPECTED_SPARSE_KEYS.items():
+            if len(doc["sparse_embed"]) == 0:
+                return False
+            for item in doc["sparse_embed"]:
+                if key not in item or not isinstance(item[key], expected_type):
+                    return False
+    
     return True
 
 
 def test_mcp(es, doc_path, body):
 
     # create test index
-    test_index_name = "test_mcp_0"
+    test_index_name = "test-mcp-0"
     create_index(es, test_index_name, body)
     add_docs_to_index(es, test_index_name, doc_path)
 
@@ -124,6 +170,7 @@ def test_mcp(es, doc_path, body):
 
             
 if __name__ == "__main__":
+
     import argparse
 
     parser = argparse.ArgumentParser(description="Elasticsearch Index Builder")
@@ -143,12 +190,26 @@ if __name__ == "__main__":
                         "name": { "type": "text" },
                         "description": { "type": "text" }
                     }
+                },
+                "sparse_embed": {
+                    "type": "nested",
+                    "properties": {
+                        "index": { "type": "integer" },
+                        "value": { "type": "float" }
+                    }
                 }
             }
         },
     }
 
-    es = Elasticsearch("http://search.es.nb.com:9200")
+    ES_HOST = "https://10.12.160.250:9200"  # could also be https://localhost:9200
+    ES_USERNAME = "elastic"
+    ES_PASSWORD = "dDkBoU_+qPhooa5FOXja"
+    es = Elasticsearch(
+        ES_HOST,
+        basic_auth=(ES_USERNAME, ES_PASSWORD),
+        verify_certs=False  # Set to False if using self-signed certs in dev
+    )
     if not es.ping():
         print("Unable to connect to Elasticsearch.")
         exit()
@@ -161,8 +222,9 @@ if __name__ == "__main__":
 
     if args.run_tests:
         test_mcp(es, doc_path, BODY)
+
     else:
-        index_name = "emily-mcp-index"
+        index_name = "test-mcp-1"
         if args.index_name:
             index_name = args.index_name
         
